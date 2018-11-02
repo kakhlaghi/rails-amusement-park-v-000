2fd7ff56b8dbb9956922bbcdfa8e2cf7e84c319f
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-02 22:59:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require 'pry'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
-  #before_action :require_logged_in, except: [:new, :create, :home]
+  before_action :require_logged_in, except: [:new, :create, :home]
 
     def logged_in?
       !!current_user
