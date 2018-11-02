46f3e305b65e2e973c30a47f863c633691e538c9
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-02 22:54:40 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
   def create
     #"name"=>"Amy Poehler", "password"=>"password", "height"=>"58", "tickets"=>"15", "happiness"=>"3", "nausea"=>"2"
-    binding.pry
     @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
