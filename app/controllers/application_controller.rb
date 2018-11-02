c60e91afc8df8b81b1013f26059b8265d87410fa
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-02 22:45:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   before_action :current_user
   before_action :require_logged_in, except: [:new, :create, :home]
 
+  def logged_in?
+      !!current_user
+    end
+  
+    private
+      def require_logged_in
+        redirect_to root_path unless logged_in?
+      end
+
+
 private
   def current_user
     session[:user_id] = params[:user_id]
