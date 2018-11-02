1cb27811451ee0cce1162cc6d4eed6080f813f08
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-02 22:22:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,6 @@ class SessionsController < ApplicationController
     end
 
     def create
-      binding.pry
       @user = User.find_by(name: params[:user][:name])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
