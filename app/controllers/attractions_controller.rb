02362abd400fd00fa62dd0310ce7cc58c53b1609
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-03 00:14:27 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,21 +1,21 @@
 class AttractionsController < ApplicationController
   before_action :set_attraction, only: [:show, :edit, :update, :destroy]
-  
+
     def index
       @attractions = Attraction.all
     end
-  
+
     def show
       @ride = Ride.new
     end
-  
+
     def new
       @attraction = Attraction.new
     end
-  
+
     def edit
     end
-  
+
     def create
       @attraction = Attraction.new(attraction_params)
       respond_to do |format|
@@ -26,7 +26,7 @@ class AttractionsController < ApplicationController
         end
       end
     end
-  
+
     def update
       respond_to do |format|
         if @attraction.update(attraction_params)
@@ -36,19 +36,19 @@ class AttractionsController < ApplicationController
         end
       end
     end
-  
+
     def destroy
       @attraction.destroy
       respond_to do |format|
         format.html { redirect_to attractions_url, notice: 'Attraction was successfully destroyed.' }
       end
     end
-  
+
     private
       def set_attraction
         @attraction = Attraction.find(params[:id])
       end
-  
+
       def attraction_params
         params.require(:attraction).permit(
           :name,
