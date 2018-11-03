7feb4aeba449be0185e85f8e10e2e9e488b13cce
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-03 01:15:59 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#create'
   root 'static#home'
   delete "/signout", to: "sessions#destroy"
-  post '/rides/new', to: "rides/new"
+  post '/rides/new', to: "rides#new"
 end
